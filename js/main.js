a328b0ddc204575d764fda55f4a36e2d84c5b880
alli-Codes <allisonevans081@gmail.com>
2023-01-02 11:58:35 +0100

    Updating the js file

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const sideBar = document.querySelector(".menu");
 const closeBtn = document.querySelector("#open");
 const openBtn = document.querySelector("#close");
+const links = document.querySelectorAll(".menu-nav a");
 
 const toggleMenu = function () {
   sideBar.classList.toggle("open");
 };
 
-openBtn.addEventListener("click", toggleMenu);
-closeBtn.addEventListener("click", toggleMenu);
+const togglingElements = [openBtn, closeBtn];
+togglingElements.forEach((element) =>
+  element.addEventListener("click", toggleMenu)
+);
+links.forEach((link) => link.addEventListener("click", toggleMenu));
